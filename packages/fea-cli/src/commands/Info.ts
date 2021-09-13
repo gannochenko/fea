55f5836c792f21d687cfc78b344a292e54e047f3
Sergei <gannochenko.sv@gmail.com>
2021-09-13 16:25:46 +0200

    wip

--- packages/fea-cli/src/commands/Info.ts
+++ packages/fea-cli/src/commands/Info.ts
@@ -6,6 +6,10 @@ import {
     Implements,
     CommandArgumentsType,
 } from './type';
+import { Git } from '../lib/Git';
+import { getBranchOrFail, getRemoteOrFail } from '../lib/utils';
+import { RC } from '../lib/RC';
+import { GitHub } from '../lib/GitHub';
 
 const d = debug('info');
 
@@ -14,9 +18,7 @@ export class Info implements CommandInstance {
     static command = 'info';
     static alias = 'i';
     static description = `Display information about the currently active feature`;
-    static options: Command['options'] = [
-        // ['-o, --output <path>', 'Output file'],
-    ];
+    static options: Command['options'] = [];
 
     constructor(
         private application: Application,
@@ -25,6 +27,83 @@ export class Info implements CommandInstance {
     ) {}
 
     async execute() {
+        const git = new Git();
+
+        const branch = await getBranchOrFail(git);
+        const remoteInfo = await getRemoteOrFail(git);
+
+        const config = await RC.getConfig();
+
+        const formatTicket = (ticketId: string) => {
+            if (config.ticketViewURLTemplate) {
+                return config.ticketViewURLTemplate.replace(
+                    '#TICKET_ID#',
+                    ticketId,
+                );
+            }
+
+            return ticketId;
+        };
+
+        // eslint-disable-next-line no-console
+        console.log(`Feature info:
+
+    Branch: ${branch.name}
+    Ticket: ${formatTicket(branch.description!.id)}`);
+
+        d('Remote info', remoteInfo);
+
+        if (!remoteInfo) {
+            return;
+        }
+
+        const github = new GitHub();
+        const pr = await github.getPRByBranch(
+            branch.name,
+            config.developmentBranch,
+            remoteInfo,
+        );
+
+        const getPRStatus = () => {
+            const result: string[] = [];
+
+            if (pr) {
+                if (pr.state === 'open') {
+                    result.push('open');
+                }
+
+                if (pr.draft) {
+                    result.push('draft');
+                }
+
+                if (pr.mergeable_state === 'unstable') {
+                    result.push('checking');
+                }
+
+                if (pr.mergeable_state === 'clean') {
+                    result.push('ready');
+                }
+
+                if (pr.mergeable_state === 'blocked') {
+                    result.push('blocked');
+                }
+            }
+
+            return result.join(', ');
+        };
+
+        if (!pr) {
+            // eslint-disable-next-line no-console
+            console.log(`
+No Pull Request info available. The feature is not yet submitted or was already merged.`);
+            return;
+        }
+
+        // eslint-disable-next-line no-console
+        console.log(`    PR:     ${pr.html_url}
+    Status: ${getPRStatus()}
+`);
+
         d('Executed successfully');
     }
 }
